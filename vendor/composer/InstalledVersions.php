a11496cb0e50dc05efb5fae3e1efc3b909010296
Md. Nahid Hasan Limon <41167616+NahidHasanLimon@users.noreply.github.com>
2021-12-26 05:06:30 +0600

    require package removed

--- vendor/composer/InstalledVersions.php
+++ vendor/composer/InstalledVersions.php
@@ -30,20 +30,11 @@ private static $installed = array (
     'aliases' => 
     array (
     ),
-    'reference' => '69b9a7fe92300d422f6ac21834580338081f1270',
+    'reference' => 'a5c2119091dab4bf10dceca34cc76b37e870d499',
     'name' => 'nahidhasanlimonlimon/testautodepoloy',
   ),
   'versions' => 
   array (
-    'monolog/monolog' => 
-    array (
-      'pretty_version' => '2.0.2',
-      'version' => '2.0.2.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => 'c861fcba2ca29404dc9e617eedd9eff4616986b8',
-    ),
     'nahidhasanlimonlimon/testautodepoloy' => 
     array (
       'pretty_version' => 'dev-main',
@@ -51,23 +42,7 @@ private static $installed = array (
       'aliases' => 
       array (
       ),
-      'reference' => '69b9a7fe92300d422f6ac21834580338081f1270',
-    ),
-    'psr/log' => 
-    array (
-      'pretty_version' => '1.1.4',
-      'version' => '1.1.4.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => 'd49695b909c3b7628b6289db5479a1c204601f11',
-    ),
-    'psr/log-implementation' => 
-    array (
-      'provided' => 
-      array (
-        0 => '1.0.0',
-      ),
+      'reference' => 'a5c2119091dab4bf10dceca34cc76b37e870d499',
     ),
   ),
 );
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,8 +22,6 @@ class ComposerAutoloaderInit3e1f4fc5dae728dc650479d69513f5dc
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInit3e1f4fc5dae728dc650479d69513f5dc', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInit3e1f4fc5dae728dc650479d69513f5dc', 'loadClassLoader'));
