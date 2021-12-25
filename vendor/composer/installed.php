c93af025e5b39add5c6fffcb58d6379f366c7c63
Md. Nahid Hasan Limon <41167616+NahidHasanLimon@users.noreply.github.com>
2021-12-26 04:54:30 +0600

    added composer .json

---
+++ vendor/composer/installed.php
@@ -0,0 +1,49 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '7e7f427f1db1caea3fc01b1757e3b6eefb8d4436',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '7e7f427f1db1caea3fc01b1757e3b6eefb8d4436',
+    ),
+    'monolog/monolog' => 
+    array (
+      'pretty_version' => '2.0.2',
+      'version' => '2.0.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c861fcba2ca29404dc9e617eedd9eff4616986b8',
+    ),
+    'psr/log' => 
+    array (
+      'pretty_version' => '1.1.4',
+      'version' => '1.1.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd49695b909c3b7628b6289db5479a1c204601f11',
+    ),
+    'psr/log-implementation' => 
+    array (
+      'provided' => 
+      array (
+        0 => '1.0.0',
+      ),
+    ),
+  ),
+);
